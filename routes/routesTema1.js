39dc299f454965f2c629e5eb53c569adb05a6014
adan-local <adangonmar@gmail.com>
2025-01-09 00:25:00 -0600

    Adding tema1-2 and github link

--- routes/routesTema1.js
+++ routes/routesTema1.js
@@ -7,4 +7,8 @@ routesTema1.get('/1', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', '1-1.html'));
 });
 
+routesTema1.get('/2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', '1-2.html'));
+});
+
 module.exports = routesTema1;
